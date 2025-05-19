565c3bb07042178609fc3bbdb03c45d92ec3440e
tuan9242 <tuanvuanh103@gmail.com>
2025-05-19 12:09:41 +0700

    Add files

--- Lab3/Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ Lab3/Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15fb5823c0e8897a7e38bba5440fba43f983dd67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77354f33f8e056d9565491c687da48d8fe391589")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
